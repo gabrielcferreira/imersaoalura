20e9157f4ffd922d450208b18e103ca2155a94ab
Gabriel <gvcostaf@gmail.com>
2024-11-25 11:29:37 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+
+// Cria uma instância do Express, que será o nosso servidor web
+const app = express();
+routes(app)
+
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console quando estiver pronto
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+
